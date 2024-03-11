e860c01e41f3aec5efefc35e7315db0c9ec1d31a
ntoiti <fidesluckia36@gmail.com>
2024-03-11 13:05:41 +0300

    first commit

--- discount.py
+++ discount.py
@@ -1,5 +1,5 @@
 def calculate_discount(price,discount_percent):
-    if discount_percent >= 20:
+    if discount_percent >=20:
         discount_amount=price*(discount_percent/100)
         final_price=price-discount_amount
         return final_price
@@ -9,7 +9,7 @@ price=float(input("enter price:"))
 discount_percent=float(input("enter discount_percent:"))
 final_price=calculate_discount(price,discount_percent)
 if final_price==price:
-        print("no dicount applied.Final price:",final_price)
+        print("no discount applied:",final_price)
 else:
          print("Final price after applying discount:",final_price)
 
